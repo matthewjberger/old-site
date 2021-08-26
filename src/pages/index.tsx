41f90f2c0291f450484e03f38dee5bb0944194f4
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-08-25 21:59:02 -0600

    Add eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Container } from "../components/Container";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
-import { Footer } from "../components/Footer";
+import { Container } from "../components/Container"
+import { DarkModeSwitch } from "../components/DarkModeSwitch"
+import { Footer } from "../components/Footer"
 
 const Index = () => (
   <Container height="100vh">
     <DarkModeSwitch />
     <Footer></Footer>
   </Container>
-);
+)
 
-export default Index;
+export default Index
