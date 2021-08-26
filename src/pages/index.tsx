c1919403f95d1545c288243dad96492d5d506d0e
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-08-25 21:47:56 -0600

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../components/Container";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Footer } from "../components/Footer";
+
+const Index = () => (
+  <Container height="100vh">
+    <DarkModeSwitch />
+    <Footer></Footer>
+  </Container>
+);
+
+export default Index;
